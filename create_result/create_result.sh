300132779dfe057d239913d5458a739298c8c662
byounghak <jindotgea36@gmail.com>
2021-07-14 13:59:38 +0900

    add Compliance Testing (TEST01)

--- create_result/create_result.sh
+++ create_result/create_result.sh
@@ -1,94 +1,55 @@
 #!/bin/bash
-DATA_DIR=/opt/data/Dataset/imn/val_50000/
-WORK_DIR=/home/tanssg/mlperf-inference-submodule/vision/classification_and_detection
-MLPERF_DIR=/home/tanssg/mlperf-inference-submodule
-SUBMIT_ORGANIZATION="LTechKorea"
-SYSTEM="L4210S-10G"
-SUBMISSION_DIR=/opt/data/$SUBMIT_ORGANIZATION
-DEVISION="closed"
-FRAMEWORK="tf"
-DEVICE="gpu"
-BENCHMARK="resnet50"
-BENCHMARK_LIST="resnet50 dlrm"
-SCENARIO_LIST="Offline Server"
-OPT="--count 100 --time 30 --qps 200 --max-latency 0.1"
+. ./env.sh
 
-## make code, complinace, measurements, results, systems dir
-mkdir $SUBMISSION_DIR
-mkdir $SUBMISSION_DIR/$DEVISION
-mkdir $SUBMISSION_DIR/$DEVISION/code
-mkdir $SUBMISSION_DIR/$DEVISION/compliance
-mkdir $SUBMISSION_DIR/$DEVISION/measurements
-mkdir $SUBMISSION_DIR/$DEVISION/results
-mkdir $SUBMISSION_DIR/$DEVISION/systems
-
-# compliance subdir
-test_sut='TEST01 TEST04-A TEST04-B TEST05'
-mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/
-for bmt_dir in $BENCHMARK_LIST
-do
-    mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir
-    for scenario in $SCENARIO_LIST
-    do
-        for n in $test_sut
-        do
-            mkdir -p $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/$n
-        done
-        mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/TEST01/performance
-        mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/TEST01/accuracy
-        mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/TEST04-A/performance
-        mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/TEST04-B/performance
-        mkdir $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/TEST05/performance
-
-    done
-done
-
-#measurements subdir
-mkdir $SUBMISSION_DIR/$DEVISION/measurements/$SUBMIT_ORGANIZATION-$SYSTEM/
-for bmt_dir in $BENCHMARK_LIST
-do
-    for scenario in $SCENARIO_LIST
-    do
-        mkdir -p $SUBMISSION_DIR/$DEVISION/measurements/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario
-    done
-done
-
-#results subdir
-mkdir -p $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/
-for bmt_dir in $BENCHMARK_LIST
-do
-    for scenario in $SCENARIO_LIST
-    do
-        mkdir -p $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario
-        mkdir -p $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/accuracy
-        mkdir -p $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$bmt_dir/$scenario/performance
-    done
-done
-
-
-
-PERF_RUN_COUNT='1'              #performance BMT run count
+#performance benchmark run count
+PERF_RUN_COUNT='1'              
 SET=$(seq 1 $PERF_RUN_COUNT)
 
+# SCENARIO_LIST 변수에 지정된 시나리오 실행(Performance, Accuracy)
 for scenario in $SCENARIO_LIST
 do
     cd $WORK_DIR
     echo '============= Performance Benchmark ================='
+    # Performance 벤치마크 실행 (PERF_RUN_COUNT 변수에 정의된만큼 반복)
     for n in $SET
     do
-        echo "Performance Benchamkr run_$n"
+        echo "Performance Benchamrk run_$n"
+        # performance 벤치마크 실행
         ./run_and_time.sh $FRAMEWORK $BENCHMARK $DEVICE --scenario $scenario $OPT
-        mkdir $SUBMISSION_DIR/$DEVISION/$SUBMIT_ORGANIZATION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/performance/run_$n
-
-        mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/* $SUBMISSION_DIR/$DEVISION/$SUBMIT_ORGANIZATION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/performance/run_$n
+        # 벤치마크 결과 제출디렉토리 생성
+        mkdir -p $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/performance/run_$n
+        # mlperf_log_summary.txt, mlperf_log_detail.txt 제줄 디렉토리로 이동
+        mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/mlperf_log_summary.txt $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/performance/run_$n/
+        mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/mlperf_log_detail.txt $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/performance/run_$n/
     done
 
     echo '==================== Accuracy ======================='
+    # Accuracy 벤치마크 실행
     ./run_and_time.sh $FRAMEWORK $BENCHMARK $DEVICE --scenario $scenario $OPT --accuracy
-     
+    
+    # accuracy.txt 생성
     python3 $WORK_DIR/tools/accuracy-imagenet.py --mlperf-accuracy-file $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/mlperf_log_accuracy.json --imagenet-val-file $DATA_DIR/val_map.txt > $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/accuracy.txt
-     
-    mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/* $SUBMISSION_DIR/$DEVISION/$SUBMIT_ORGANIZATION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/accuracy/
+    
+    # mlperf_log_summary.txt, mlperf_log_detail.txt, mlperf_log_accuracy.json 제출디렉토리로 이동
+    mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/mlperf_log_summary.txt $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/accuracy/
+    mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/mlperf_log_detail.txt $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/accuracy/
+    mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/mlperf_log_accuracy.json $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/accuracy/
+    mv $WORK_DIR/output/$FRAMEWORK-$DEVICE/$BENCHMARK/accuracy.txt $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario/accuracy/
 done
 
+echo '==================TEST01========================'
+# Compliance Testing TEST01
+cp $MLPERF_DIR/compliance/audit_v0.5/nvidia/TEST01/audit.config $WORK_DIR
+cd $WORK_DIR
+for scenario in $SCENARIO_LIST
+do
+    # audit.config 로 실행
+    #./run_and_time.sh $FRAMEWORK $BENCHMARK $DEVICE --scenario $scenario $OPT --user_conf audit.config
 
+    # run_verification.py 로 audit.config로 실행한것과 기존테스트 비교
+    python3 $MLPERF_DIR/compliance/nvidia/TEST01/run_verification.py \
+        -r $SUBMISSION_DIR/$DEVISION/results/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario \
+        -c $WORK_DIR/output/tf-gpu/resnet50 \
+        -o $SUBMISSION_DIR/$DEVISION/compliance/$SUBMIT_ORGANIZATION-$SYSTEM/$BENCHMARK/$scenario
+done
+rm $WORK_DIR/audit.config
